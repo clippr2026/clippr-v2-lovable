ab75f250a77805f8bd14da2a4eb4c0d74477d1fe
clippr2026 <aurostyloadmi@gmail.com>
2026-06-20 11:01:10 -0300

    Add files via upload

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -62,10 +62,10 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, overlayClassName, hideOverlay, ...props }, ref) => (
-  <SheetPortal>
+>(({ side = "right", className, children, overlayClassName, hideOverlay, forceMount, ...props }, ref) => (
+  <SheetPortal forceMount={forceMount}>
     {!hideOverlay && <SheetOverlay className={overlayClassName} />}
-    <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
+    <SheetPrimitive.Content ref={ref} forceMount={forceMount} className={cn(sheetVariants({ side }), className)} {...props}>
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background cursor-pointer transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
